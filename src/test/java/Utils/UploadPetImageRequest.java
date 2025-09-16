f096154e18f7853405335323e4ddb6e6f2daaf7d
Sumeet Raina <sumeet@Mac.broadband>
2025-09-16 16:44:16 +0100

    Added tests for status codes 200, 400, and 404

--- src/test/java/Utils/UploadPetImageRequest.java
+++ src/test/java/Utils/UploadPetImageRequest.java
@@ -17,4 +17,25 @@ public class UploadPetImageRequest {
       .POST(HttpRequest.BodyPublishers.ofFile(Path.of("src/test/resources/test-image.png")))
       .build();
   }
+
+  public static HttpRequest uploadImageRequestWithoutFile() throws URISyntaxException {
+
+    return HttpRequest.newBuilder()
+      .uri(new URI("https://petstore3.swagger.io/api/v3/pet/10/uploadImage"))
+      .header("accept", "application/json")
+      .header("Content-Type", "application/octet-stream")
+      .POST(HttpRequest.BodyPublishers.noBody())
+      .build();
+  }
+
+  public static HttpRequest uploadImageRequestInvalidId() throws URISyntaxException, FileNotFoundException {
+
+    return HttpRequest.newBuilder()
+      .uri(new URI("https://petstore3.swagger.io/api/v3/pet/9999999/uploadImage"))
+      .header("accept", "application/json")
+      .header("Content-Type", "application/octet-stream")
+      .POST(HttpRequest.BodyPublishers.ofFile(Path.of("src/test/resources/test-image.png")))
+      .build();
+  }
+
 }
