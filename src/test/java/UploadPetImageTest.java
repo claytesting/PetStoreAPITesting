e9e7b3356a2f5ddca746e14f5f1d31f119ae92da
Sumeet Raina <sumeet@Mac.broadband>
2025-09-17 11:43:00 +0100

    Updated tests to use pet pojo

--- src/test/java/UploadPetImageTest.java
+++ src/test/java/UploadPetImageTest.java
@@ -1,3 +1,4 @@
+import pojos.Pet;
 import utils.UploadPetImageRequest;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,7 +14,7 @@ import java.net.http.HttpResponse;
 
 public class UploadPetImageTest {
   private static HttpResponse<String> response;
-  private static JsonNode jsonBody;
+  private static Pet petResponse;
 
   @BeforeAll
   static void setup() {
@@ -28,7 +29,7 @@ public class UploadPetImageTest {
       System.out.println("Body: " + response.body());
 
       ObjectMapper mapper = new ObjectMapper();
-      jsonBody = mapper.readTree(response.body());
+      petResponse = mapper.readValue(response.body(), Pet.class);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -44,29 +45,28 @@ public class UploadPetImageTest {
   @Test
   @DisplayName("Response contains correct pet ID")
   void uploadImageByPetId_ResponseContainsPetId() {
-    MatcherAssert.assertThat(jsonBody.get("id").asInt(), Matchers.is(10));
+    MatcherAssert.assertThat(petResponse.getId(), Matchers.is(10));
   }
 
   @Test
   @DisplayName("Response contains non-empty photoUrls")
   void uploadImageByPetId_ResponseContainsPhotoUrls() {
     MatcherAssert.assertThat(
-      jsonBody.get("photoUrls").isArray() && !jsonBody.get("photoUrls").isEmpty(),
-      Matchers.is(true)
+      petResponse.getPhotoUrls() != null && !petResponse.getPhotoUrls().isEmpty(), Matchers.is(true)
     );
   }
 
   @Test
   @DisplayName("Response contains tag name playful")
   void uploadImageByPetId_ResponseContainsTagPlayful() {
-    MatcherAssert.assertThat(jsonBody.get("tags").get(0).get("name").asText(), Matchers.is("string"));
+  MatcherAssert.assertThat(petResponse.getTags().getFirst().getName(), Matchers.is("string"));
   }
 
 
   @Test
   @DisplayName("Response contains non-empty photoUrls")
   void uploadImageByPetId_ResponseContainsStatusAvailable() {
-    MatcherAssert.assertThat(jsonBody.get("status").asText(), Matchers.is("available")
+    MatcherAssert.assertThat(petResponse.getStatus(), Matchers.is("available")
     );
   }
 
