587c742cfe2383c65e571a2406242b896dedc368
Oliver Felgate <ofelgate@spartaglobal.com>
2025-09-18 12:55:16 +0100

    reorganise classes by endpoint

---
+++ src/test/java/unhappyPath/petEndpoint/UploadPetImageNoFileTest.java
@@ -0,0 +1,43 @@
+package unhappyPath.petEndpoint;
+
+import Utils.UploadPetImageRequest;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static utils.PetShopAPI.setupPetForQuery;
+
+public class UploadPetImageNoFileTest {
+  private static HttpResponse<String> response;
+
+  @BeforeAll
+  static void setup() {
+    Integer petId = setupPetForQuery();
+    try {
+      HttpClient client = HttpClient.newHttpClient();
+
+      HttpRequest request = UploadPetImageRequest.uploadImageRequestWithoutFile(petId);
+
+      response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+      System.out.println("Status: " + response.statusCode());
+      System.out.println("Body: " + response.body());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @Test
+  @DisplayName("Given no file return response code 400")
+  void uploadImageByPetId_NoFileData_ReturnCode400() {
+    MatcherAssert.assertThat(response.statusCode(), Matchers.is(400));
+  }
+
+}
---
+++ src/test/java/unhappyPath/userEndpoint/GetUserByUsernameUnhappyTest.java
@@ -0,0 +1,55 @@
+package unhappyPath.userEndpoint;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static utils.PetShopAPI.getUserByUsernameNoPathParamRequestSpec;
+import static utils.PetShopAPI.getUserByUsernameRequestSpec;
+
+public class GetUserByUsernameUnhappyTest {
+
+    private static Response response;
+
+    static void setup(RequestSpecification spec) {
+        response = RestAssured
+                .given(spec)
+                .when()
+                .get()
+
+                .then()
+                .extract().response();
+    }
+
+    @Test
+    @DisplayName("Given a valid Username as query param return 405 code")
+    void getUserByUsername_ValidUsername_ReturnCode405() {
+        setup(getUserByUsernameNoPathParamRequestSpec()
+                .queryParam("username", "user1"));
+
+        MatcherAssert.assertThat(response.statusCode(), Matchers.is(405));
+    }
+
+    @Test
+    @DisplayName("Given a username that doesn't exist return 404 code")
+    void getUserByUsername_UsernameDoesNotExist_ReturnCode404() {
+        setup(getUserByUsernameRequestSpec()
+                .pathParam("username", "usercoolmccoolface"));
+
+        MatcherAssert.assertThat(response.statusCode(), Matchers.is(404));
+    }
+
+    @Test
+    @DisplayName("Given an invalid username return 400 code")
+    void getUserByUsername_ValidUsername_ReturnCode200() {
+        setup(getUserByUsernameRequestSpec()
+                .pathParam("username", "1"));
+
+        MatcherAssert.assertThat(response.statusCode(), Matchers.is(400));
+    }
+}
